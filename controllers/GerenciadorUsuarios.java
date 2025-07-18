33c2ee20cfbb5dfc455a79e8526a33b20a2bf675
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-07-18 11:55:39 -0300

    gerenciador melhorado

--- controllers/GerenciadorUsuarios.java
+++ controllers/GerenciadorUsuarios.java
@@ -3,20 +3,30 @@ import java.util.ArrayList;
 import models.Usuario;
 
 
-public class gerenciadorUsuario {
+public class GerenciadorUsuarios {
    private ArrayList<Usuario> usuarios = new ArrayList<>();
 
    public void adicionarUsuario(String nome){
       usuarios.add(new Usuario(nome));
    }
 
-   public ArrayList<Usuario> listaUsuarios() {
+   public ArrayList<Usuario> listarUsuarios() {
        return usuarios;
    }
 
-   public boolean removeUsuario(String nome) {
+   public boolean removerUsuario(String nome) {
       return usuarios.removeIf(u -> u.getName().equals(nome));
    }
+
+   public Usuario buscarUsuarioPorNome(String nome) {
+    for (Usuario u : usuarios) {
+        if (u.getName().equalsIgnoreCase(nome)) {
+            return u;
+        }
+    }
+    return null;
+}
+
 }
 
 
