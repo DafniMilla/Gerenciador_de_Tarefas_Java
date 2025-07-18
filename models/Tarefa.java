f3ecdcc98c34733d4054df6040bd384ad251ed94
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-07-18 11:53:52 -0300

    models tarefas adicionado

--- models/Tarefa.java
+++ models/Tarefa.java
@@ -1,19 +1,28 @@
 package models;
 
-public class Tarefa {
-    private String descricao;
-    private Usuario usuario;
+public class Tarefa extends Item{
+    private String status;
+    private Usuario encarregado;
 
-    public Tarefa(String descricao, Usuario usuario){
-        this.descricao=descricao;
-        this.usuario=usuario;
-    }
-    public Usuario getUsuario(){
-        return usuario;
+    public Tarefa(String titulo, String descricao, Usuario encarregado){
+        super(titulo,descricao);
+        this.status = "Pendente";
+        this.encarregado = encarregado;
     }
 
-    public String getDescricao(){
-        return descricao;
+    public String getStatus(){
+        return status;
     }
-    
+
+    public void setStatus(Status novoStatus){
+        this.status = novoStatus.toString();
+    }
+
+    public Usuario getEncarregado() {
+        return encarregado;
+    }
+    @Override
+    public void mostrarDetalhes(){
+        System.out.println("Tarefa:"+ getTitulo() + "Status" + status + "Descrição" + getDescricao() + "Encarregado" + encarregado.getName()) ;
+    }
 }
